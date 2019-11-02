fb91b0a651f1c8396d158830dd290024987f7912
Michael Sloan <mgsloan@gmail.com>
2019-11-02 01:37:04 -0600

    Initial more polished code + package scaffolding

---
+++ changelog.md
@@ -0,0 +1,3 @@
+# Changelog for unordered-apply
+
+## Unreleased changes
--- src/Control/UnorderedApply.hs
+++ src/Control/UnorderedApply.hs
@@ -6,156 +6,183 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE ConstraintKinds #-}
 
 module Control.UnorderedApply
   (
-    -- * Functions for type directed curry which matches first parameter
-    applyFirst1
-  , applyFirstN
-    -- * Functions for type directed curry which matches unique parameter
-  , apply1
-  , applyN
-    -- * Machinery for type directed curry which matches first parameter
-  , CheckMatchFirst
-    -- * Machinery for type directed curry which matches unique parameter
-  , CheckAmbiguity
-  , HasOneMatch
-  , HasNoMatch
+    -- * Type-directed function application which uses first match
+    applyByType
+  , reorderArgs
+  , (?)
+    -- * Type-directed function application which requires unique match
+  , applyByUniqueType
+  , reorderUniqueArgs
+    -- * Machinery for type directed function application
+  , MatchArgResult(..)
+  , MatchFirstArg
+  , HasAMatch
+  , ApplyByType(ApplyByTypeResult)
+    -- * Machinery for unique match function application
+  , HasUniqueMatch
+  , CheckAmbiguousMatch
+    -- * Machinery for reordering arguments
+  , HasArgResult(..)
+  , HasArg
+  , ReorderArgs
+  , ReorderUniqueArgs
   ) where
 
-import Data.Proxy
+import Data.Proxy (Proxy(..))
 import GHC.TypeLits (TypeError, ErrorMessage(..))
 
-data MatchMode = Unique | First
+applyByType, (?)
+  :: forall matches a f.
+     ( matches ~ HasAMatch a f f
+     , ApplyByType matches a f
+     )
+  => f -> a -> ApplyByTypeResult matches a f
+applyByType = applyByTypeImpl (Proxy :: Proxy matches)
+{-# INLINE applyByType #-}
 
-data MatchResult = NoMatchFirst | NoMatch | AmbiguousMatch | Match
+infixl 1 ?
+(?) = applyByType
+{-# INLINE (?) #-}
 
-type family CheckMatchFirst (mode :: MatchMode) f a :: MatchResult where
-  CheckMatchFirst 'Unique (a -> r) a = CheckAmbiguity r a
-  CheckMatchFirst 'First (a -> r) a = 'Match
-  CheckMatchFirst _ (a -> r) b = 'NoMatchFirst
-  CheckMatchFirst _ _ _ = 'NoMatch
+applyByUniqueType
+  :: forall matches a f.
+     ( matches ~ HasUniqueMatch a f f
+     , ApplyByType matches a f
+     )
+  => f -> a -> ApplyByTypeResult matches a f
+applyByUniqueType = applyByTypeImpl (Proxy :: Proxy matches)
+{-# INLINE applyByUniqueType #-}
 
-type family CheckAmbiguity f a :: MatchResult where
-  CheckAmbiguity (a -> r) a = 'AmbiguousMatch
-  CheckAmbiguity (a -> r) b = CheckAmbiguity r b
-  CheckAmbiguity _ _ = 'Match
+reorderArgs :: forall f g. ReorderArgs (HasArg f) f g => f -> g
+reorderArgs = reorderArgsImpl (Proxy :: Proxy (HasArg f))
 
--- TODO: explain
-type family HasOneMatch f0 f a :: MatchResult where
-  HasOneMatch f0 (a -> r) a = HasNoMatch f0 r a
-  HasOneMatch f0 (a -> r) b = HasOneMatch f0 r b
-  HasOneMatch f0 _ a = TypeError (NoMatchError f0 a)
+reorderUniqueArgs :: forall f g. ReorderUniqueArgs (HasArg f) f g => f -> g
+reorderUniqueArgs = reorderUniqueArgsImpl (Proxy :: Proxy (HasArg f))
 
-type family HasNoMatch f0 f a :: MatchResult where
-  HasNoMatch f0 (a -> r) a = TypeError (AmbiguousError f0 a)
-  HasNoMatch f0 (a -> r) b = HasNoMatch f0 r b
-  HasNoMatch f0 _ a = CheckMatchFirst 'Unique f0 a
+--------------------------------------------------------------------------------
+-- Match machinery
 
-class TypeDirectedCurry (mode :: MatchMode) (matches :: MatchResult) f a where
-  type Result mode matches f a
-  applyInternal
-    :: Proxy mode
-    -> Proxy matches
+data MatchArgResult
+  = Matches
+  | Doesn'tMatch
+  | NoArgToMatch
+
+type family MatchFirstArg a f :: MatchArgResult where
+  MatchFirstArg a (a -> r) = 'Matches
+  MatchFirstArg a (b -> r) = 'Doesn'tMatch
+  MatchFirstArg _ _ = 'NoArgToMatch
+
+type family HasAMatch a f f0 :: MatchArgResult where
+  HasAMatch a (a -> r) f0 = MatchFirstArg a f0
+  HasAMatch a (b -> r) f0 = HasAMatch a r f0
+  HasAMatch a _ f0 = TypeError (NoMatchError a f0)
+
+class ApplyByType (matches :: MatchArgResult) a f where
+  type ApplyByTypeResult matches a f
+  applyByTypeImpl
+    :: Proxy matches
     -> f
     -> a
-    -> Result mode matches f a
+    -> ApplyByTypeResult matches a f
 
-instance TypeDirectedCurry mode 'Match (a -> r) a where
-  type Result mode 'Match (a -> r) a = r
-  applyInternal _ _ f x = f x
-  {-# INLINE applyInternal #-}
+instance ApplyByType 'Matches a (a -> r) where
+  type ApplyByTypeResult 'Matches a (a -> r) = r
+  applyByTypeImpl _ f x = f x
+  {-# INLINE applyByTypeImpl #-}
 
-instance TypeDirectedCurry mode (CheckMatchFirst mode r a) r a =>
-         TypeDirectedCurry mode 'NoMatchFirst (b -> r) a where
-  type Result mode 'NoMatchFirst (b -> r) a =
-    b -> Result mode (CheckMatchFirst mode r a) r a
-  applyInternal mode _ f y =
-    \x -> applyInternal mode (Proxy :: Proxy (CheckMatchFirst mode r a)) (f x) y
-  {-# INLINE applyInternal #-}
+instance ApplyByType (MatchFirstArg a r) a r
+      => ApplyByType 'Doesn'tMatch a (b -> r) where
+  type ApplyByTypeResult 'Doesn'tMatch a (b -> r) =
+    b -> ApplyByTypeResult (MatchFirstArg a r) a r
+  applyByTypeImpl _ f y =
+    \x -> applyByTypeImpl (Proxy :: Proxy (MatchFirstArg a r)) (f x) y
+  {-# INLINE applyByTypeImpl #-}
 
-type AmbiguousError f a =
-  'Text "Parameter type " ':<>:
-  'ShowType a ':<>:
-  'Text " occurs multiple times in the arguments of the function type " ':$$:
-  'Text "  " ':<>: 'ShowType f ':$$:
-  'Text "and so cannot be applied via type directed currying."
+instance TypeError (NoMatchForResultError a r) => ApplyByType 'NoArgToMatch a r where
+  type ApplyByTypeResult 'NoArgToMatch a r = TypeError (NoMatchForResultError a r)
+  applyByTypeImpl = error "impossible"
 
-instance TypeError (AmbiguousError (b -> r) a) => TypeDirectedCurry mode 'AmbiguousMatch (b -> r) a where
-  type Result mode 'AmbiguousMatch (b -> r) a = TypeError (AmbiguousError (b -> r) a)
-  applyInternal = error "impossible"
+--------------------------------------------------------------------------------
+-- Unique match machinery
 
-type NoMatchError f a =
+type family HasUniqueMatch a f f0 :: MatchArgResult where
+  HasUniqueMatch a (a -> r) f0 = CheckAmbiguousMatch a r f0
+  HasUniqueMatch a (b -> r) f0 = HasUniqueMatch a r f0
+  HasUniqueMatch a _ f0 = TypeError (NoMatchError a f0)
+
+type family CheckAmbiguousMatch a f f0 :: MatchArgResult where
+  CheckAmbiguousMatch a (a -> r) f0 = TypeError (AmbiguousMatchError a f0)
+  CheckAmbiguousMatch a (b -> r) f0 = CheckAmbiguousMatch a r f0
+  CheckAmbiguousMatch a _ f0 = MatchFirstArg a f0
+
+--------------------------------------------------------------------------------
+-- Argument reordering machinery
+
+data HasArgResult = ArgPresent | NoArg
+
+type family HasArg f :: HasArgResult where
+  HasArg (_ -> _) = 'ArgPresent
+  HasArg _ = 'NoArg
+
+class ReorderArgs (fHasArg :: HasArgResult) f g where
+  reorderArgsImpl :: Proxy fHasArg -> f -> g
+
+instance
+    ( matches ~ HasAMatch a (b -> y) (b -> y)
+    , result ~ ApplyByTypeResult matches a (b -> y)
+    , ApplyByType matches a (b -> y)
+    , ReorderArgs (HasArg result) result x
+    ) => ReorderArgs 'ArgPresent (b -> y) (a -> x) where
+  reorderArgsImpl _ f x =
+    reorderArgsImpl (Proxy :: Proxy (HasArg result)) $
+    applyByType f x
+  {-# INLINE reorderArgsImpl #-}
+
+instance r1 ~ r2 => ReorderArgs 'NoArg r1 r2 where
+  reorderArgsImpl _ f = f
+  {-# INLINE reorderArgsImpl #-}
+
+class ReorderUniqueArgs (fHasArg :: HasArgResult) f g where
+  reorderUniqueArgsImpl :: Proxy fHasArg -> f -> g
+
+instance
+    ( matches ~ HasUniqueMatch a (b -> y) (b -> y)
+    , result ~ ApplyByTypeResult matches a (b -> y)
+    , ApplyByType matches a (b -> y)
+    , ReorderUniqueArgs (HasArg result) result x
+    ) => ReorderUniqueArgs 'ArgPresent (b -> y) (a -> x) where
+  reorderUniqueArgsImpl _ f x =
+    reorderUniqueArgsImpl (Proxy :: Proxy (HasArg result)) $
+    applyByUniqueType f x
+  {-# INLINE reorderUniqueArgsImpl #-}
+
+instance r1 ~ r2 => ReorderUniqueArgs 'NoArg r1 r2 where
+  reorderUniqueArgsImpl _ f = f
+  {-# INLINE reorderUniqueArgsImpl #-}
+
+--------------------------------------------------------------------------------
+-- Type error messages
+
+type NoMatchError a f =
   'Text "Parameter type " ':<>:
   'ShowType a ':<>:
   'Text " does not occur in the arguments of the function type " ':$$:
   'Text "  " ':<>: 'ShowType f ':$$:
-  'Text "and so cannot be applied via type directed currying."
+  'Text "and so cannot be applied via type directed application."
 
-instance TypeError (NoMatchError f a) => TypeDirectedCurry mode 'NoMatch f a where
-  type Result mode 'NoMatch f a = TypeError (NoMatchError f a)
-  applyInternal = error "impossible"
+type NoMatchForResultError a r =
+  'Text "Parameter type " ':<>:
+  'ShowType a ':<>:
+  'Text " does not occur in the arguments of the function that returns " ':$$:
+  'Text "  " ':<>: 'ShowType r ':$$:
+  'Text "and so cannot be applied via type directed application."
 
-apply1
-  :: forall matches f a.
-     ( matches ~ HasOneMatch f f a
-     , TypeDirectedCurry 'Unique matches f a
-     )
-  => f -> a -> Result 'Unique matches f a
-apply1 = applyInternal (Proxy :: Proxy 'Unique) (Proxy :: Proxy matches)
-{-# INLINE apply1 #-}
-
-applyFirst1
-  :: forall matches f a.
-     ( matches ~ CheckMatchFirst 'First f a
-     , TypeDirectedCurry 'First matches f a
-     )
-  => f -> a -> Result 'First matches f a
-applyFirst1 = applyInternal (Proxy :: Proxy 'First) (Proxy :: Proxy matches)
-{-# INLINE applyFirst1 #-}
-
-applyN :: forall f g. MultiCurry 'Unique (IsMore f) f g => f -> g
-applyN = multiCurry (Proxy :: Proxy 'Unique) (Proxy :: Proxy (IsMore f))
-{-# INLINE applyN #-}
-
-applyFirstN :: forall f g. MultiCurry 'First (IsMore f) f g => f -> g
-applyFirstN = multiCurry (Proxy :: Proxy 'First) (Proxy :: Proxy (IsMore f))
-{-# INLINE applyFirstN #-}
-
-data MultiCurryState = More | Done
-
-class MultiCurry (mode :: MatchMode) (isFunction :: MultiCurryState) f g where
-  multiCurry :: Proxy mode -> Proxy isFunction -> f -> g
-
-instance
-    ( matches ~ HasOneMatch (a -> x) (a -> x) b
-    , result ~ Result 'Unique matches (a -> x) b
-    , TypeDirectedCurry 'Unique matches (a -> x) b
-    , MultiCurry 'Unique (IsMore result) result y
-    ) => MultiCurry 'Unique 'More (a -> x) (b -> y) where
-  multiCurry mode _ f x =
-    multiCurry mode (Proxy :: Proxy (IsMore result)) $
-    apply1 f x
-  {-# INLINE multiCurry #-}
-
-instance
-    ( matches ~ CheckMatchFirst 'First (a -> x) b
-    , result ~ Result 'First matches (a -> x) b
-    , TypeDirectedCurry 'First matches (a -> x) b
-    , MultiCurry 'First (IsMore result) result y
-    ) => MultiCurry 'First 'More (a -> x) (b -> y) where
-  multiCurry mode _ f x =
-    multiCurry mode (Proxy :: Proxy (IsMore result)) $
-    applyFirst1 f x
-  {-# INLINE multiCurry #-}
-
--- instance MultiCurry 'False r r where
-instance r1 ~ r2 => MultiCurry mode 'Done r1 r2 where
-  multiCurry _ _ f = f
-  {-# INLINE multiCurry #-}
-
-type family IsMore f where
-  IsMore (_ -> _) = 'More
-  IsMore _ = 'Done
+type AmbiguousMatchError a f =
+  'Text "Parameter type " ':<>:
+  'ShowType a ':<>:
+  'Text " occurs multiple times in the arguments of the function type " ':$$:
+  'Text "  " ':<>: 'ShowType f ':$$:
+  'Text "and so cannot be applied via type directed application which requires unique match."
