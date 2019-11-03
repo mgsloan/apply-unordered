7b079643c647aa988df2c15989bfb95355cb6874
Michael Sloan <mgsloan@gmail.com>
2019-11-02 21:56:43 -0600

    Add a simple test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,34 @@
+{-# OPTIONS_GHC -fdefer-type-errors -Wno-deferred-type-errors #-}
+
+module Main where
+
+import Control.UnorderedApply
+import Test.Hspec
+import Test.ShouldNotTypecheck (shouldNotTypecheck)
+
+replicateChar :: Int -> Char -> String
+replicateChar = replicate
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Type directed apply with first match" $ do
+    it "handles simple parameters (1)" $
+      (replicateChar ? 'a' ? 3) `shouldBe` "aaa"
+    it "handles simple parameters (2)" $
+      (replicateChar ? 3 ? 'a') `shouldBe` "aaa"
+    it "handles simple ambiguous parameters (1)" $
+      ((||) ? True ? False) `shouldBe` True
+    it "handles simple ambiguous parameters (2)" $
+      ((||) ? False ? False) `shouldBe` False
+    it "errors when there is no match" $
+      shouldNotTypecheck ((||) ? "test")
+
+  describe "Type directed apply with unique match" $ do
+    it "handles simple parameters (1)" $
+      (replicateChar ?! 'a' ?! 3) `shouldBe` "aaa"
+    it "handles simple parameters (2)" $
+      (replicateChar ?! 3 ?! 'a') `shouldBe` "aaa"
+    it "checks for ambiguity" $
+      shouldNotTypecheck ((||) ?! True ?! False :: Bool)
+    it "errors when there is no match" $
+      shouldNotTypecheck (replicateChar ? "test")
