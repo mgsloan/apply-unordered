52ffa8d62ee0bea98d3a7dd828b5cfd68f68aa7b
Michael Sloan <mgsloan@gmail.com>
2019-11-02 22:19:42 -0600

    Tests for how polymorphism is handled

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,25 +9,35 @@ import Test.ShouldNotTypecheck (shouldNotTypecheck)
 replicateChar :: Int -> Char -> String
 replicateChar = replicate
 
+polymorphicReturn :: Int -> Char -> Maybe a
+polymorphicReturn _ _ = Nothing
+
+three :: Int
+three = 3
+
 main :: IO ()
 main = hspec $ do
   describe "Type directed apply with first match" $ do
     it "handles simple parameters (1)" $
-      (replicateChar ? 'a' ? 3) `shouldBe` "aaa"
+      (replicateChar ? 'a' ? three) `shouldBe` "aaa"
     it "handles simple parameters (2)" $
-      (replicateChar ? 3 ? 'a') `shouldBe` "aaa"
+      (replicateChar ? three ? 'a') `shouldBe` "aaa"
     it "handles simple ambiguous parameters (1)" $
       ((||) ? True ? False) `shouldBe` True
     it "handles simple ambiguous parameters (2)" $
       ((||) ? False ? False) `shouldBe` False
     it "errors when there is no match" $
       shouldNotTypecheck ((||) ? "test")
+    it "doesn't handle any parameter polymorphism" $
+      shouldNotTypecheck (replicate ? 3 $ 'a' :: String)
+    it "does handle result polymorphism" $
+      (polymorphicReturn ? three $ 'a') `shouldBe` Nothing
 
   describe "Type directed apply with unique match" $ do
     it "handles simple parameters (1)" $
-      (replicateChar ?! 'a' ?! 3) `shouldBe` "aaa"
+      (replicateChar ?! 'a' ?! three) `shouldBe` "aaa"
     it "handles simple parameters (2)" $
-      (replicateChar ?! 3 ?! 'a') `shouldBe` "aaa"
+      (replicateChar ?! three ?! 'a') `shouldBe` "aaa"
     it "checks for ambiguity" $
       shouldNotTypecheck ((||) ?! True ?! False :: Bool)
     it "errors when there is no match" $
