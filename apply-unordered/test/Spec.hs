34ee01f1f631331c0e70360b67389d803ca0efff
Michael Sloan <mgsloan@gmail.com>
2019-11-05 01:11:46 -0700

    Add test for re-ordering functions of 3 args

--- apply-unordered/test/Spec.hs
+++ apply-unordered/test/Spec.hs
@@ -12,16 +12,19 @@ replicateChar = replicate
 polymorphicReturn :: Int -> Char -> Maybe a
 polymorphicReturn _ _ = Nothing
 
-three :: Int
-three = 3
+threeArgs :: Int -> Char -> Bool -> String
+threeArgs a b c = show (a, b, c)
+
+i3 :: Int
+i3 = 3
 
 main :: IO ()
 main = hspec $ do
   describe "Type directed apply with first match" $ do
     it "handles simple parameters (1)" $
-      (replicateChar ? 'a' ? three) `shouldBe` "aaa"
+      (replicateChar ? 'a' ? i3) `shouldBe` "aaa"
     it "handles simple parameters (2)" $
-      (replicateChar ? three ? 'a') `shouldBe` "aaa"
+      (replicateChar ? i3 ? 'a') `shouldBe` "aaa"
     it "handles simple ambiguous parameters (1)" $
       ((||) ? True ? False) `shouldBe` True
     it "handles simple ambiguous parameters (2)" $
@@ -31,14 +34,20 @@ main = hspec $ do
     it "doesn't handle any parameter polymorphism" $
       shouldNotTypecheck (replicate ? 3 $ 'a' :: String)
     it "does handle result polymorphism" $
-      (polymorphicReturn ? three $ 'a') `shouldBe` Nothing
+      (polymorphicReturn ? i3 $ 'a') `shouldBe` Nothing
 
   describe "Type directed apply with unique match" $ do
     it "handles simple parameters (1)" $
-      (replicateChar ?! 'a' ?! three) `shouldBe` "aaa"
+      (replicateChar ?! 'a' ?! i3) `shouldBe` "aaa"
     it "handles simple parameters (2)" $
-      (replicateChar ?! three ?! 'a') `shouldBe` "aaa"
+      (replicateChar ?! i3 ?! 'a') `shouldBe` "aaa"
     it "checks for ambiguity" $
       shouldNotTypecheck ((||) ?! True ?! False :: Bool)
     it "errors when there is no match" $
       shouldNotTypecheck (replicateChar ? "test")
+
+  describe "Argument reordering" $ do
+    it "can re-order a 3 argument function (1)" $
+      reorderArgs threeArgs 'a' i3 True `shouldBe` "(3,'a',True)"
+    it "can re-order a 3 argument function (2)" $
+      reorderArgs threeArgs False 'b' i3 `shouldBe` "(3,'b',False)"
